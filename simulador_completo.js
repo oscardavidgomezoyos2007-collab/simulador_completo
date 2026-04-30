4214abd2a3aa041119c56248a4782bb2423b637b
oscardavidgomezoyos2007 <oscardavidgomezoyos2007@gmail.com>
2026-04-29 21:06:21 -0500

    simulador_completo

---
+++ simulador_completo.js
@@ -0,0 +1,32 @@
+let clientes = [];
+  let creditos = [];
+ 
+  let tasaInteres = 15;
+  let clienteSeleccionado = null;
+  let cuotaCalculada = 0;
+  let montoCalculado = 0;
+  let plazoCalculado = 0;
+  let creditoAprobado = false;
+ 
+function ocultarSecciones(){
+  let componente= document.getElementById("parametros");
+  let listaClass = componente.classList;
+  listaClass.remove("activa");  // oculta
+ 
+  let componente2 = document.getElementById("clientes");
+  let listaClass2 = componente2.classList;
+  listaClass2.remove("activa");
+}
+ 
+ 
+function mostrarSeccion(id){
+  ocultarSecciones();
+ 
+  let componente= document.getElementById(id);
+  let listaClass = componente.classList;
+  listaClass.add("activa");  //activa
+}
+ 
+ 
+//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+ 
