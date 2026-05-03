f712f869121ebe0aaee64052e5d69f957c1c4671
oscardavidgomezoyos2007 <oscardavidgomezoyos2007@gmail.com>
2026-05-03 13:57:05 -0500

    cambios 1 parte

--- simulador_completo.js
+++ simulador_completo.js
@@ -27,6 +27,49 @@ function mostrarSeccion(id){
   listaClass.add("activa");  //activa
 }
  
- 
+function guardarTasa() {
+  let ImputTaza = recuperarInt('tasaInteres')
+  if (ImputTaza > 10 && ImputTaza < 20  ) {
+    tasaInteres = ImputTaza;
+    mostrarTexto('mensajeTasa','tazaConfigurada') 
+
+  }else {
+    mostrarTexto('mensajeTasa','tazamalconfigurada')
+  }
+  
+}
+
+function guardarClientes() {
+  let Cedula = recuperaraTexto("idCedula");
+  let Nombre = recuperaraTexto("idNombre");
+  let Apellido = recuperaraTexto("idApellido");
+  let Ingresos= recuperarFloat("idIngresos");
+  let Egresos= recuperarFloat("idEgresos")
+
+  let index = clientes.findIndex(c => c.Cedula === Cedula);
+    if (index === -1) {
+        clientes.push({ Cedula, Nombre, Apellido, Ingresos, Egresos });
+    } else {
+        clientes[index] = { Cedula, Nombre, Apellido, Ingresos, Egresos };
+    } 
+   pintarClientes();
+}
+
+
+ function pintarClientes() {
+   let arreglo = document.getElementById("tablaClientes");
+   arreglo.innerHTML=""; 
+   clientes.forEach(c => { 
+    arreglo.innerHTML += `<tr>
+            <td>${c.Cedula}</td><td>${c.Nombre}</td><td>${c.Ingresos}</td><td>${c.Egresos}</td>
+            <td> <button> Actualizar </button> </td>
+        </tr>`; 
+   } 
+
+   )
+
+ }
+
+
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
  
